cb910a94addd0056572997fce01c8361c904072f
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2023-02-11 11:42:56 +0530

    Update Q4InventoryClass.java

--- Q4InventoryClass.java
+++ Q4InventoryClass.java
@@ -17,7 +17,7 @@ public void setDetails(String specification,double cost,int count){
 }
 public void getDetails()
 {
-    System.out.print("\n\n THE PRODUCT DETAILS ARE ::\nProduct Name:"+this.name+"\nCost:"+this.cost+"\n");
+    System.out.print("\nProduct Name:"+this.name+"\nCost:"+this.cost+"\n");
             System.out.print("Product Count:"+this.count+"\nProduct Specification:"+this.specification+"\n\n");
 }
   public int getCount() {
@@ -46,52 +46,84 @@ class Inventory {
   }
 }
 
-public class Q4InventoryClass {
+public class InventoryClass {
   static Scanner input = new Scanner(System.in);
   static Inventory inventory = new Inventory();
 
   static void viewProductList() {
+    int i=1;
     System.out.println("\n\nProduct List:");
     ArrayList<Product> products = inventory.getProducts();
     for (Product product : products) {
-      System.out.println(product.name);
+      System.out.print("Product : "+i);
+      product.getDetails();
+      i+=1;
     }
     System.out.println("\n\n");
   }
   static void viewProductCount() {
     System.out.print("Enter product name: ");
     String j=input.next();
+    int c=0;
     ArrayList<Product> products = inventory.getProducts();
     for (Product product : products) 
         if (j.equals(product.name)) {
           System.out.println("\n\n\n The Quantity of "+product.name+" : "+product.getCount()+"\n\n\n");
+          c=1;
           break;
           }
+          if(c==0)
+          System.out.println("\nProduct not present in the Inventory\n\n");
 }
 static void viewProductDetails(){
     System.out.print("Enter product name: ");
     String j=input.next();
+    int c=0;
     ArrayList<Product> products = inventory.getProducts();
     for (Product product : products) 
         if (j.equals(product.name)) {
+          System.out.println("Product Detailsare:");
             product.getDetails();
+            c=1;
             break;
         }
+        if(c==0)
+          System.out.println("\nProduct not present in the Inventory\n\n");
 }
 static void editProduct(){
     System.out.print("\n\nEnter product name you want ot change details: ");
     String j=input.next();
+    int c=0;
     ArrayList<Product> products = inventory.getProducts();
     for (Product product : products) 
         if (j.equals(product.name)) {
-            System.out.println("\nenter  specification,cost and count for product");
-            String specification=input.next();
-            double cost=input.nextDouble();
-            int  count=input.nextInt();
+          String specification=product.specification;
+    double cost=product.cost;
+    int count=product.count;
+            System.out.println("\nselect to change 1.Specification\n2.Cost\n3.Count");
+            int op=input.nextInt();
+            if(op==1)
+            {
+              System.out.println("Enter new Specification:\n");
+              specification=input.next();
+            }
+            else if(op==2)
+            {
+              System.out.println("Enter New Cost:");
+              cost=input.nextDouble();
+            }
+            else if(op==3)
+            {
+              System.out.println("Enter New  Cost:");
+              count=input.nextInt();
+            } 
             product.setDetails(specification, cost, count);
             System.out.println("Product details are changed successfully\n\n");
+            c=1;
             break;
 }
+if(c==0)
+          System.out.println("\nProduct not present in the Inventory\n\n");
 }
 static void updateInventory(){
     System.out.print("\n\nSelect  1.To add product\n2.To delete Product ");
@@ -114,6 +146,7 @@ static void updateInventory(){
         for (Product product : products) 
             if (j.equals(product.name)) { 
                 inventory.removeProduct(product); 
+                System.out.println("Product is Removed from Inventory\n");
                 break;
     }
     }   
@@ -125,12 +158,7 @@ static void updateInventory(){
     inventory.addProduct(new Product("OPPO","4G,64GB STORAGE",7999, 45));
     inventory.addProduct(p);
     do {
-      System.out.println("Inventory Management System");
-      System.out.println("1. Product List");
-      System.out.println("2. Product Count");
-      System.out.println("3. View Product Details");
-      System.out.println("4. Edit Product");
-      System.out.println("5. Update Inventory");
+      System.out.println("Inventory Management System\n1. ProductList\n2. Product Count\n3. View Product Details\n4. Edit Product\n5. Update Inventory");
       System.out.print("Enter your choice: ");
       choice = input.nextInt();
       switch (choice) {
